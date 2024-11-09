0e3bb997ca33ce8684985a0e618f29c6e68311f8
nmhillusion <nguyenminhhieu.geek@gmail.com>
2024-11-09 14:12:52 +0700

    update init script

--- backend/src/main/resources/startup/init-db-scheme.sql
+++ backend/src/main/resources/startup/init-db-scheme.sql
@@ -1,6 +1,21 @@
 
 create table t_cx_customer (
-  id int primary key,
+  customer_id int primary key,
   full_name nvarchar(200),
   id_card_number nvarchar(30)
+);
+
+create table t_cx_commodity_type (
+  type_id int primary key,
+  type_name nvarchar(100)
+);
+
+create table t_cx_commodity (
+  com_id int primary key,
+  com_name nvarchar(200),
+  com_type int
+);
+
+create table t_cx_transaction(
+  trans_id int primary key
 );
