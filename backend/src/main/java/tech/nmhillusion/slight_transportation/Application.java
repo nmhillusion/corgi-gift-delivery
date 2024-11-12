80a024b7d867e34cd9c1143bd853a9ac7cbb686f
nmhillusion <nguyenminhhieu.geek@gmail.com>
2024-11-12 07:08:53 +0700

    add anno of n2mix

--- backend/src/main/java/tech/nmhillusion/slight_transportation/Application.java
+++ backend/src/main/java/tech/nmhillusion/slight_transportation/Application.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import tech.nmhillusion.n2mix.annotation.EnableN2mix;
 import tech.nmhillusion.slight_transportation.startup.DatabaseSchemeSeeder;
 
 import java.util.Calendar;
@@ -21,6 +22,7 @@ import static tech.nmhillusion.n2mix.helper.log.LogHelper.getLogger;
                 HibernateJpaAutoConfiguration.class
         }
 )
+@EnableN2mix
 @SpringBootApplication
 public class Application implements CommandLineRunner {
 
