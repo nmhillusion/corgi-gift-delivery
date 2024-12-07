767a779e5591ea0530ef1d2be4c5295fb213d209
nmhillusion <nguyenminhhieu.geek@gmail.com>
2024-12-07 10:08:11 +0700

    update menu styling

--- frontend/src/app/layout/menu/menu.component.ts
+++ frontend/src/app/layout/menu/menu.component.ts
@@ -1,12 +1,13 @@
 import { Component, Input } from "@angular/core";
 import { MatIconModule } from "@angular/material/icon";
+import { RouterModule } from "@angular/router";
 import { Menu } from "@app/model/menu.model";
 
 @Component({
   selector: "app-menu",
   templateUrl: "./menu.component.html",
   styleUrls: ["./menu.component.scss"],
-  imports: [MatIconModule],
+  imports: [MatIconModule, RouterModule],
 })
 export class MenuComponent {
   @Input("data") data!: Menu;
