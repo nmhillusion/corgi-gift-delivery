273e0931522d19d31d66eeae37833df7761aa3da
nmhillusion <nguyenminhhieu.geek@gmail.com>
2025-09-03 22:12:56 +0700

    impl export for delivery return

--- backend/src/main/java/tech/nmhillusion/corgi_gift_delivery/domains/deliveryReturn/DeliveryReturnServiceImpl.java
+++ backend/src/main/java/tech/nmhillusion/corgi_gift_delivery/domains/deliveryReturn/DeliveryReturnServiceImpl.java
@@ -1,20 +1,28 @@
 package tech.nmhillusion.corgi_gift_delivery.domains.deliveryReturn;
 
+import org.apache.commons.lang3.stream.Streams;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.Resource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.web.multipart.MultipartFile;
 import tech.nmhillusion.corgi_gift_delivery.annotation.TransactionalService;
+import tech.nmhillusion.corgi_gift_delivery.domains.delivery.DeliveryService;
+import tech.nmhillusion.corgi_gift_delivery.domains.deliveryReturnStatus.DeliveryReturnStatusService;
+import tech.nmhillusion.corgi_gift_delivery.entity.business.DeliveryEntity;
 import tech.nmhillusion.corgi_gift_delivery.entity.business.DeliveryReturnEntity;
+import tech.nmhillusion.corgi_gift_delivery.entity.business.DeliveryReturnStatusEntity;
+import tech.nmhillusion.corgi_gift_delivery.helper.BeanHelper;
 import tech.nmhillusion.corgi_gift_delivery.service.business.AbstractBaseDeliveryService;
 import tech.nmhillusion.corgi_gift_delivery.service.core.SequenceService;
 import tech.nmhillusion.n2mix.exception.ApiResponseException;
-import tech.nmhillusion.n2mix.exception.MissingDataException;
 import tech.nmhillusion.n2mix.exception.NotFoundException;
 import tech.nmhillusion.n2mix.helper.log.LogHelper;
+import tech.nmhillusion.n2mix.helper.office.excel.writer.ExcelWriteHelper;
+import tech.nmhillusion.n2mix.helper.office.excel.writer.model.BasicExcelDataModel;
 
-import java.io.IOException;
 import java.text.MessageFormat;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -27,10 +35,12 @@ import java.util.Optional;
 @TransactionalService
 public class DeliveryReturnServiceImpl extends AbstractBaseDeliveryService<DeliveryReturnEntity, DeliveryReturnSearchDto, DeliveryReturnRepository> implements DeliveryReturnService {
     private final DeliveryReturnExcelSheetParser deliverReturnExcelSheetParser;
+    private final BeanHelper beanHelper;
 
-    protected DeliveryReturnServiceImpl(DeliveryReturnRepository repository, SequenceService sequenceService, DeliveryReturnExcelSheetParser deliverReturnExcelSheetParser) {
+    protected DeliveryReturnServiceImpl(DeliveryReturnRepository repository, SequenceService sequenceService, DeliveryReturnExcelSheetParser deliverReturnExcelSheetParser, BeanHelper beanHelper) {
         super(repository, sequenceService);
         this.deliverReturnExcelSheetParser = deliverReturnExcelSheetParser;
+        this.beanHelper = beanHelper;
     }
 
     @Override
@@ -99,6 +109,56 @@ public class DeliveryReturnServiceImpl extends AbstractBaseDeliveryService<Deliv
 
     @Override
     public Resource export(DeliveryReturnSearchDto deliveryReturnSearchDto) throws ApiResponseException {
-        throw new UnsupportedOperationException();
+        try {
+            final long totalElements = getTotalElementsForSearch(deliveryReturnSearchDto);
+
+            final Page<DeliveryReturnEntity> resultPage = search(deliveryReturnSearchDto, 0, (int) totalElements);
+
+            final DeliveryService deliveryService = beanHelper.injectForService(DeliveryService.class);
+            final DeliveryReturnStatusService deliveryReturnStatusService = beanHelper.injectForService(DeliveryReturnStatusService.class);
+
+            final byte[] byteData = new ExcelWriteHelper()
+                    .addSheetData(
+                            new BasicExcelDataModel()
+                                    .setSheetName("DeliveryReturns")
+                                    .setHeaders(
+                                            List.of(
+                                                    Streams.of(DeliveryReturnParserEnum.values())
+                                                            .map(DeliveryReturnParserEnum::getColumnName)
+                                                            .toList()
+                                            )
+                                    )
+                                    .setBodyData(
+                                            resultPage.getContent()
+                                                    .stream()
+                                                    .map(it -> {
+                                                        final List<String> dRow = new ArrayList<>();
+
+                                                        final DeliveryEntity delivery = deliveryService.getById(
+                                                                String.valueOf(it.getDeliveryId())
+                                                        );
+
+                                                        dRow.add(delivery.getEventId());
+                                                        dRow.add(delivery.getCustomerId());
+                                                        dRow.add(
+                                                                Optional.ofNullable(deliveryReturnStatusService.getDeliveryReturnStatusByStatusId(
+                                                                                String.valueOf(it.getReturnStatusId())
+                                                                        ))
+                                                                        .map(DeliveryReturnStatusEntity::getStatusName)
+                                                                        .orElse("")
+                                                        );
+                                                        dRow.add(it.getNote());
+
+                                                        return dRow;
+                                                    })
+                                                    .toList()
+                                    )
+                    )
+                    .build();
+
+            return new ByteArrayResource(byteData);
+        } catch (Throwable ex) {
+            throw new ApiResponseException(ex);
+        }
     }
 }
