2bf0ce57bebaf917ee8eb9eb0581312fd8e10121
nmhillusion <nguyenminhhieu.geek@gmail.com>
2025-10-04 21:49:46 +0700

    update script for cross platform

--- build.sh
+++ build.sh
@@ -8,6 +8,15 @@ echo "Frontend base href: $frontend_base_href"
 
 cd frontend
 
-OLD_PATTERN="var__frontend_base_href"
-sed "s|$OLD_PATTERN|$frontend_base_href|g" "./package.template.json" > "./package.json"
-npm run build
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    echo "Build on Linux"
+    var__frontend_base_href=$frontend_base_href npm run build:unix
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "Build on macOS"
+    var__frontend_base_href=$frontend_base_href npm run build:unix
+elif [[ "$OSTYPE" == "cygwin" ]] || [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "win32" ]]; then
+    echo "Build on Windows"
+    var__frontend_base_href=$frontend_base_href npm run build:win
+else
+    echo "Unknown OS"
+fi
